6b9e62a0fb4d3b10da35611542c6213630050945
sloev <johannesgj@gmail.com>
2014-03-27 23:25:19 +0100

    sad

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import pystrix
+
+if __name__ == '__main__':
+    agi = pystrix.agi.AGI()
+
+    agi.execute(pystrix.agi.core.Answer()) #Answer the call
+
+    response = agi.execute(pystrix.agi.core.StreamFile('demo-thanks', escape_digits=('1', '2'))) #Play a file; allow DTMF '1' or '2' to interrupt
+    if response: #Playback was interrupted; if you don't care, you don't need to catch this
+        (dtmf_character, offset) = response #The key pressed by the user and the playback time
+
+    agi.execute(pystrix.agi.core.Hangup()) #Hang up the call
