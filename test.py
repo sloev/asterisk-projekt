6f5af340508a1cb8823ce86e66ee282a9638bad5
sloev <johannesgj@gmail.com>
2014-04-02 14:28:53 +0200

    lol

--- test.py
+++ test.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python
 import pystrix
+from crontab import CronTab
+
 
 if __name__ == '__main__':
     agi = pystrix.agi.AGI()
 
     agi.execute(pystrix.agi.core.Answer()) #Answer the call
-
-    response = agi.execute(pystrix.agi.core.StreamFile('demo-thanks', escape_digits=('1', '2'))) #Play a file; allow DTMF '1' or '2' to interrupt
-    if response: #Playback was interrupted; if you don't care, you don't need to catch this
-        (dtmf_character, offset) = response #The key pressed by the user and the playback time
-
-    agi.execute(pystrix.agi.core.Hangup()) #Hang up the call
+    myDate={}
+    myDateIndices={0:"month",1:"day",2:"hour",3:"minute"}
+    boundaries={0:[1,12],1:[1,31],2:[0,23],3:[0,59]}
+    success=True
+    for i in range(0,3):
+	    response = agi.execute(pystrix.agi.core.StreamFile('./custom/'+myDateIndices[i])) #Play a file; allow DTMF '1' or '2' to interrupt
+	    myDate[myDateIndices[i]] = 10 * agi.execute(pystrix.agi.core.WaitForDigit(timeout=-1)
+	    myDate[myDateIndices[i]] = myDate[myDateIndices[i]] + agi.execute(pystrix.agi.core.WaitForDigit(timeout=-1)
+	    bound=boundaries[i]
+	    if not bound[0]<=myDate[myDateIndices[i]] <=bound[1]:
+		    response = agi.execute(pystrix.agi.core.StreamFile('./custom/numberError')) 
+		    success=False
+		    break		 
+		
+	print myDate
+	if success:
+		mydateString=str(myDate["month"])+str(myDate["day"])+str(myDate["hour"])+str(myDate["minute"])
+	    response = agi.execute(pystrix.agi.core.StreamFile('./custom/recordMessage') #indtal besked og tryk 0
+	    response = agi.execute(pystrix.agi.core.RecordFile('./messages/'+mydateString+".wav", format='wav', escape_digits='0', timeout=15000)
+	    response = agi.execute(pystrix.agi.core.StreamFile('./custom/thankYou')) 
+	    
+	    #cron stuff
+		job  = cron.new(command='/usr/bin/echo')
+		job.month.on(myDate["month"])
+		job.day.on(myDate["day"])
+		job.hour.on(myDate["hour"])
+		job.minute.on(myDate["minute"])
+		
+	
+	    
+	agi.execute(pystrix.agi.core.Hangup())
